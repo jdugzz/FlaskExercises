d11763ae87955bce79197c9b8b3e5feb51c48569
jdugzz <97847249+jdugzz@users.noreply.github.com>
2023-02-07 19:34:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import Question, Survey, satisfaction_survey
 
@@ -12,20 +12,34 @@ responses = []
 
 @app.route('/')
 def home_page():
-    return render_template('base.html', satisfaction_survey=satisfaction_survey)
+    return render_template('base.html', survey=satisfaction_survey)
 
-@app.route('/start', methods=['POST'])
+@app.route('/start', methods=["POST"])
 def start():
-    responses = []
+    responses.clear()
     return redirect('/questions/0')
 
 @app.route("/answer", methods=['POST'])
 def question_handle():
-    choice = request.form.get['choices']
+    choice = request.form['choices']
     responses.append(choice)
-    return redirect(f'/questions/0')
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect('/finished')
+    if len(responses) > len(satisfaction_survey.questions):
+        responses.pop()
+        return redirect('/finished')
+    return redirect(f'/questions/{len(responses)}')
 
-@app.route('/questions/<int:num>', methods=['POST'])
+@app.route('/questions/<int:num>')
 def question_page(num):
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect('/finished')
+    if num != len(responses):
+        flash('Quit tampering with the URL bucko')
+        return redirect(f'/questions/{len(responses)}')
     question = satisfaction_survey.questions[num]
     return render_template('question.html',question_num = num, question = question)
+
+@app.route('/finished')
+def finished():
+    return render_template('finished.html', responses=responses)
